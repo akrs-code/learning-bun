f25260f8ae0c07d4e368fed312a63fc33c9d0def
akrs-code <abdulkhaliqsolaiman@gmail.com>
2025-12-30 21:10:29 +0800

    feat: learned how to use Bun in creating local host server

--- index.ts
+++ index.ts
@@ -1,7 +1,22 @@
-const z = 10
-const read = (text:string) => {
-    return text
-}
+import figlet from 'figlet'
 
-const message = read("Hello World!@")
-console.log(message);
+const server = Bun.serve({
+    port:4000,
+    fetch(req){
+        const url = new URL(req.url)
+
+        if(url.pathname === '/'){
+        const body = figlet.textSync("I am Learning Bun!")
+        return new Response(body)
+        }
+        if(url.pathname === '/about'){
+            return new Response("About me!")
+        }
+        if(url.pathname === '/contact'){
+            return new Response("Contact me")
+        }
+        return new Response('404!')
+    }
+})
+
+console.log(`Listening on PORT https://localhost:${server.port}`)
